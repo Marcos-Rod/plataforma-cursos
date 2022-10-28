b9a93c94ff691812286d1436b4ad9e8dbf69e183
Marcos <tmlwar01@gmail.com>
2022-10-28 12:27:43 -0500

    Home Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function __invoke()
+    {
+        $courses = Course::where('status', '3')->latest('id')->get();
+
+        return view('welcome', compact('courses'));
+    }
 }
--- app/Models/Course.php
+++ app/Models/Course.php
@@ -10,66 +10,88 @@ class Course extends Model
     use HasFactory;
 
     protected $guarded = ['id', 'status'];
-    
+    protected $withCount = ['students', 'reviews'];
+
     const BORRADOR = 1;
     const REVISION = 2;
     const PUBLICADO = 3;
 
+    public function getRatingAttribute()
+    {
+
+        if ($this->reviews_count) {
+            return round($this->reviews->avg('rating'), 1);
+        } else {
+            return 5;
+        }
+    }
+
     //Relacio uno a muchos
-    public function reviews(){
+    public function reviews()
+    {
         return $this->hasMany(Review::class);
     }
 
-    public function requirements(){
+    public function requirements()
+    {
         return $this->hasMany(Requirement::class);
     }
-    
 
-    public function goals(){
+
+    public function goals()
+    {
         return $this->hasMany(Goal::class);
     }
-    
 
-    public function audiences(){
+
+    public function audiences()
+    {
         return $this->hasMany(Audience::class);
     }
-    
 
-    public function sections(){
+
+    public function sections()
+    {
         return $this->hasMany(Section::class);
     }
-    
+
     //Relacio uno a muchos inversa
 
-    public function teacher(){
+    public function teacher()
+    {
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function level(){
+    public function level()
+    {
         return $this->belongsTo(Level::class);
     }
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Level::class);
     }
 
-    public function price(){
+    public function price()
+    {
         return $this->belongsTo(Level::class);
     }
 
     //Relacion muchos a muchos
-    public function students(){
+    public function students()
+    {
         return $this->belongsToMany(User::class);
     }
-    
+
     //Relacion uno a uno polimorfica
-    public function image(){
+    public function image()
+    {
         return $this->morphOne(Image::class, 'imageable');
     }
 
     //Relacion de cursos con lecciones (entre tablas intermedias)
-    public function lessons(){
+    public function lessons()
+    {
         return $this->hasManyThrough(Lesson::class, Section::class);
     }
-    
 }
