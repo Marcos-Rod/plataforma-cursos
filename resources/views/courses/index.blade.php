813deff875b55332539f56c714373de17d53662a
Marcos <tmlwar01@gmail.com>
2022-11-24 13:46:39 -0600

    Clases de estilos personalizadas

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-    <section class="bg-cover" style="background-image: url({{ asset('img/cursos/cursos-index.jpg') }})">
+    <section class="bg-cover" style="background-image: url('{{ asset('img/cursos/cursos-index.jpg') }}')">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2">
                 <h1 class="text-white font-bold text-4xl">Apparet statim, quae sint officia, quae actiones. </h1>
